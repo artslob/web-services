e8f72dbacc4511537489d76b7bb8476ef6a1c9b0
artslob <artslob@yandex.ru>
2019-04-19 17:30:19 +0300

    lab2: basic client is done

--- lab2/src/main/java/ifmo/artslob/webservices/lab2/client/WebServiceClient.java
+++ lab2/src/main/java/ifmo/artslob/webservices/lab2/client/WebServiceClient.java
@@ -2,25 +2,57 @@ package ifmo.artslob.webservices.lab2.client;
 
 import ifmo.artslob.webservices.lab2.client.generated.City;
 import ifmo.artslob.webservices.lab2.client.generated.CityService;
+import ifmo.artslob.webservices.lab2.client.generated.CityWebService;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
 public class WebServiceClient {
+    private final static String createMethodName = "create";
+    private final static String readMethodName = "read";
+    private final static String updateMethodName = "update";
+    private final static String deleteMethodName = "delete";
+
     public static void main(String[] args) throws MalformedURLException {
+        String method = (args.length > 0) ? args[0] : "default";
+
         URL url = new URL("http://localhost:8080/CityService?wsdl");
-        CityService cityService = new CityService(url);
-        List<City> cities = cityService.getCityWebServicePort().getCities("", "", "", "", "");
-        for (City city : cities) {
-            System.out.println("City{" +
-                    "name='" + city.getName() + '\'' +
-                    ", country='" + city.getCountry() + '\'' +
-                    ", founded=" + city.getFounded() +
-                    ", population=" + city.getPopulation() +
-                    ", area=" + city.getArea() +
-                    '}');
+        CityWebService cityWebService = new CityService(url).getCityWebServicePort();
+
+        switch (method) {
+            case createMethodName: {
+                cityWebService.createCity("", "", "", "", "");
+                break;
+            }
+            case readMethodName: {
+                List<City> cities = cityWebService.getCities("", "", "", "", "");
+                for (City city : cities) {
+                    System.out.println("City{" +
+                            "name='" + city.getName() + '\'' +
+                            ", country='" + city.getCountry() + '\'' +
+                            ", founded=" + city.getFounded() +
+                            ", population=" + city.getPopulation() +
+                            ", area=" + city.getArea() +
+                            '}');
+                }
+                System.out.println("Total cities: " + cities.size());
+                break;
+            }
+            case updateMethodName: {
+                cityWebService.updateCity("", "", "", "", "", "");
+                break;
+            }
+            case deleteMethodName: {
+                cityWebService.deleteCity("");
+                break;
+            }
+            default: {
+                System.err.println("Program requires first cli argument to be name of one CRUD method: " +
+                        createMethodName + ", " + readMethodName + ", " + updateMethodName + " or " + deleteMethodName);
+                System.exit(1);
+                break;
+            }
         }
-        System.out.println("Total cities: " + cities.size());
     }
 }
