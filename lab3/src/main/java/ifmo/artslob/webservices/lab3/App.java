f144541c372a11e2ba8f6dace6ab22f150ca58fd
artslob <artslob@yandex.ru>
2019-04-20 21:33:05 +0300

    lab3: disable stacktraces in soap-messages

--- lab3/src/main/java/ifmo/artslob/webservices/lab3/App.java
+++ lab3/src/main/java/ifmo/artslob/webservices/lab3/App.java
@@ -4,6 +4,8 @@ import javax.xml.ws.Endpoint;
 
 public class App {
     public static void main(String[] args) {
+        //disable stacktraces in soap-message
+        System.setProperty("com.sun.xml.ws.fault.SOAPFaultBuilder.disableCaptureStackTrace", "false");
         String url = "http://0.0.0.0:8080/CityService";
         Endpoint.publish(url, new CityWebService());
         System.out.println("Started on:" + url);
