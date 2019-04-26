2eb9f8947aa915e2ef0a1f7b7225ce0050fb116d
artslob <artslob@yandex.ru>
2019-04-26 23:27:29 +0300

    lab5: implement createCity method in rest service (post method)

--- lab5/src/main/java/ifmo/artslob/webservices/lab5/CityResource.java
+++ lab5/src/main/java/ifmo/artslob/webservices/lab5/CityResource.java
@@ -1,15 +1,24 @@
 package ifmo.artslob.webservices.lab5;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/cities")
 @Produces({MediaType.APPLICATION_JSON})
 public class CityResource {
+    @POST
+    public String createCity(
+            @FormParam("name") String name,
+            @FormParam("country") String country,
+            @FormParam("founded") String founded,
+            @FormParam("population") String population,
+            @FormParam("area") String area
+    ) {
+        int result = new PostgreSQLDAO(ConnectionUtil.getConnection()).createCity(name, country, founded, population, area);
+        return Integer.toString(result);
+    }
+
     @GET
     public List<City> getCities(
             @QueryParam("name") String name,
