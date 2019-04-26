58b2352813c66954dfd71193224afcb8f588b260
artslob <artslob@yandex.ru>
2019-04-26 23:29:54 +0300

    lab5: update client with create method

--- lab5/src/main/java/ifmo/artslob/webservices/lab5/client/ClientApp.java
+++ lab5/src/main/java/ifmo/artslob/webservices/lab5/client/ClientApp.java
@@ -4,26 +4,80 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 import ifmo.artslob.webservices.lab5.City;
 
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import java.util.List;
 
 public class ClientApp {
+    private final static String createMethodName = "create";
+    private final static String readMethodName = "read";
+    private final static String updateMethodName = "update";
+    private final static String deleteMethodName = "delete";
+
     private static final String URL = "http://localhost:8080/rest/cities";
 
     public static void main(String[] args) {
         Client client = Client.create();
-        printList(getCities(
-                client,
-                getCliArgOrNull(args, 0),
-                getCliArgOrNull(args, 1),
-                getCliArgOrNull(args, 2),
-                getCliArgOrNull(args, 3),
-                getCliArgOrNull(args, 4)
-        ));
-        System.out.println();
-        printList(getCities(client, "Paris", null, null, null, null));
+        String method = (args.length > 0) ? args[0] : "default";
+
+        switch (method) {
+            case createMethodName: {
+                checkParamsLength(args, 6);
+                String result = createCity(client, args[1], args[2], args[3], args[4], args[5]);
+                System.out.println("Created new city with id: " + result);
+                break;
+            }
+            case readMethodName: {
+                List<City> cities = getCities(
+                        client,
+                        getCliArgOrNull(args, 1),
+                        getCliArgOrNull(args, 2),
+                        getCliArgOrNull(args, 3),
+                        getCliArgOrNull(args, 4),
+                        getCliArgOrNull(args, 5)
+                );
+                for (City city : cities) {
+                    System.out.println(city);
+                }
+                System.out.println("Total cities: " + cities.size());
+                break;
+            }
+            default: {
+                System.err.println("Program requires first cli argument to be name of one CRUD method: " +
+                        createMethodName + ", " + readMethodName + ", " + updateMethodName + " or " + deleteMethodName);
+                System.exit(1);
+                break;
+            }
+        }
+    }
+
+    private static String createCity(
+            Client client,
+            String name,
+            String country,
+            String founded,
+            String population,
+            String area
+    ) {
+        WebResource webResource = client.resource(URL);
+        MultivaluedMap<String, String> formData = new MultivaluedMapImpl();
+        formData.add("name", name);
+        formData.add("country", country);
+        formData.add("founded", founded);
+        formData.add("population", population);
+        formData.add("area", area);
+        ClientResponse response = webResource
+                .type(MediaType.APPLICATION_FORM_URLENCODED_TYPE)
+                .post(ClientResponse.class, formData);
+        if (response.getStatus() != ClientResponse.Status.OK.getStatusCode()) {
+            throw new IllegalStateException("Request failed");
+        }
+        GenericType<String> type = new GenericType<String>() {
+        };
+        return response.getEntity(type);
     }
 
     private static List<City> getCities(
@@ -59,13 +113,11 @@ public class ClientApp {
         return response.getEntity(type);
     }
 
-    private static void printList(List<City> cities) {
-        if (cities.size() == 0) {
-            System.out.println("got empty cities list");
-            return;
-        }
-        for (City city : cities) {
-            System.out.println(city);
+    private static void checkParamsLength(String[] args, int length) {
+        if (args.length < length) {
+            String message = String.format("Wrong number of parameters, got: %d, expected: %d", args.length, length);
+            System.err.println(message);
+            System.exit(1);
         }
     }
 
